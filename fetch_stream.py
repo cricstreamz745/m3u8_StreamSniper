554f01ed109079742397ae839e7e84b2bcfad6fa
cricstreamz745 <cricketstream745@gmail.com>
2026-01-07 17:25:40 +0530

    Update source JSON URL in fetch_stream.py

--- fetch_stream.py
+++ fetch_stream.py
@@ -14,7 +14,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
-SOURCE_JSON = "https://raw.githubusercontent.com/cricstreamz745/Willow/refs/heads/main/output.json"
+SOURCE_JSON = "https://raw.githubusercontent.com/cricstreamz745/Hit-Maal/refs/heads/main/hitmall.json"
 OUTPUT_FILE = "m3u8.json"
 
 M3U8_RE = re.compile(r'https?://[^\'"\s>]+\.m3u8[^\'"\s>]*', re.I)
